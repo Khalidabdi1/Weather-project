915f992feb45cebd8d7a4e36a134929eac38729e
Khalid Abdi <xamza2009@gmail.com>
2025-07-28 19:48:48 +0300

    add i18next

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+let Weather_api=process.env.REACT_APP_WEATHER_API_KEY;
+let city="Riyadh"
+let https=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Weather_api}&units=metric&lang=ar`
+
+//api
+axios.get(https)
+.then((res)=>{
+  console.log(res.data)
+}).catch((err)=>{
+  console.log(err)
+})
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
